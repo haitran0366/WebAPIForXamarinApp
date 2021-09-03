93e480fb9ed2749383d29acbadc4615c23faa495
haitran0366 <haitran_usa@yahoo.com>
2021-09-03 13:49:51 -0700

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIForXamarinApp.Models;
+using WebAPIForXamarinApp.Services;
+
+namespace WebAPIForXamarinApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        //IUserService iUser = new UserService();
+
+        // GET all action
+        [HttpGet]
+        public ActionResult<List<UserModel>> GetAll() => UserService.GetAll();
+
+        [HttpGet("{username}/{password}")]
+        public ActionResult<UserModel> Get(string username, string password)
+        {
+            var user = UserService.GetUser(username, password);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAccount(UserModel user)
+        {
+            UserService.Add(user);
+            return CreatedAtAction(nameof(CreateAccount), new { id = user.Id }, user);
+        }
+
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIForXamarinApp.Models;
+
+namespace WebAPIForXamarinApp.Services
+{
+    public class UserService
+    {
+        static List<UserModel> Users { get; }
+
+        static UserService()
+        {
+            Users = new List<UserModel>
+            {
+                new UserModel { Id = 1, UserName = "hai123", Email = "haitran_usa@yahoo.com",Password="123456" },
+                new UserModel { Id = 2, UserName = "hai321", Email = "haitran_usa@yahoo.com",Password="654321"}
+            };
+        }
+        public static List<UserModel> GetAll() => Users;
+        public static UserModel GetUser(string userName, string password) => Users.FirstOrDefault(p => p.UserName == userName & p.Password == password);
+
+        public static void Add(UserModel user)
+        {
+            user.Id = Users.Count + 1;
+            Users.Add(user);
+
+        }
+
+    }
+}
